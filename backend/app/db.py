f72223ea0695009f046d7f3553cfc3d2b195fc55
hatembellaaj <hatem.bellaaj@redcad.org>
2026-01-24 12:45:58 +0100

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -5,6 +5,7 @@ import psycopg
 DEFAULT_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
 DATABASE_URL = os.getenv("DATABASE_URL", DEFAULT_DATABASE_URL)
 
-
 def get_connection() -> psycopg.Connection:
-    return psycopg.connect(DATABASE_URL)
+    conn = psycopg.connect(DATABASE_URL)
+    conn.autocommit = True
+    return conn
